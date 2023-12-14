6d794b3bf93eb3b24e389cd6ffdb03d2f8eb5992
Peter Hind <peter.hind47@gmail.com>
2023-12-14 23:21:41 +1300

    add private key signing func

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,16 +1,29 @@
-import server from "./server";
+import server from './server'
+import * as secp from 'ethereum-cryptography/secp256k1'
+import { keccak256 } from 'ethereum-cryptography/keccak'
+import { toHex } from 'ethereum-cryptography/utils'
 
-function Wallet({ address, setAddress, balance, setBalance }) {
+function Wallet({
+  address,
+  setAddress,
+  balance,
+  setBalance,
+  setPrivateKey,
+  privateKey,
+}) {
   async function onChange(evt) {
-    const address = evt.target.value;
-    setAddress(address);
+    const privateKey = evt.target.value
+    setPrivateKey(privateKey)
+    const address = toHex(secp.secp256k1.getPublicKey(privateKey))
+
+    setAddress(address)
     if (address) {
       const {
         data: { balance },
-      } = await server.get(`balance/${address}`);
-      setBalance(balance);
+      } = await server.get(`balance/${address}`)
+      setBalance(balance)
     } else {
-      setBalance(0);
+      setBalance(0)
     }
   }
 
@@ -19,13 +32,19 @@ function Wallet({ address, setAddress, balance, setBalance }) {
       <h1>Your Wallet</h1>
 
       <label>
-        Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
+        Private Key
+        <input
+          placeholder="Type in a private key"
+          value={privateKey}
+          onChange={onChange}
+        ></input>
       </label>
+
+      <div>Address: {address}</div>
 
       <div className="balance">Balance: {balance}</div>
     </div>
-  );
+  )
 }
 
-export default Wallet;
+export default Wallet
--- server/index.js
+++ server/index.js
@@ -1,44 +1,46 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const port = 3042;
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = 3042
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
-};
+  '02570e7faa7c19e4afec24bcd97931acb099506dc5e05acd0b3ece3cf018d9ee1a': 100,
+  '030df2ef9bd5a83f4472300d8a9ade6b949ea5537d61510c54e423cbd6a3955ac0': 50,
+  '0x3': 75,
+}
 
-app.get("/balance/:address", (req, res) => {
-  const { address } = req.params;
-  const balance = balances[address] || 0;
-  res.send({ balance });
-});
+app.get('/balance/:address', (req, res) => {
+  const { address } = req.params
+  const balance = balances[address] || 0
+  res.send({ balance })
+})
 
-app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+app.post('/send', (req, res) => {
+  // TODO: get signature and recover PubKey
 
-  setInitialBalance(sender);
-  setInitialBalance(recipient);
+  const { sender, recipient, amount } = req.body
+
+  setInitialBalance(sender)
+  setInitialBalance(recipient)
 
   if (balances[sender] < amount) {
-    res.status(400).send({ message: "Not enough funds!" });
+    res.status(400).send({ message: 'Not enough funds!' })
   } else {
-    balances[sender] -= amount;
-    balances[recipient] += amount;
-    res.send({ balance: balances[sender] });
+    balances[sender] -= amount
+    balances[recipient] += amount
+    res.send({ balance: balances[sender] })
   }
-});
+})
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}!`);
-});
+  console.log(`Listening on port ${port}!`)
+})
 
 function setInitialBalance(address) {
   if (!balances[address]) {
-    balances[address] = 0;
+    balances[address] = 0
   }
 }
